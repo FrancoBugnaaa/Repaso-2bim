69720ca2f9011c3310bb1f74799d0fbc8c1ee2ba
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-05-17 15:58:48 -0300

    done
    listo bim2

---
+++ Assets/Scripts/BrickCloner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour
+{
+    public GameObject brickPrefab;
+    public float Xoffset;
+    public Text useroffset;
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Xoffset = float.Parse(useroffset.text);
+    }
+    
+    public void CloneBrick()
+    {
+        //el if para hacer que se limitada la cantidad de cubos clonados (asi se hace con un If)
+        if(counter == 0)
+        {
+            Xoffset = float.Parse(useroffset.text);
+        }
+        if (counter <5)
+        {
+            GameObject clon;
+            clon = Instantiate(brickPrefab);
+            //sin el += se clonaria en (-3,0,0) con el +=
+            clon.transform.position += new Vector3(Xoffset, 0, 0);
+            Xoffset += 1.1f;
+            counter++;
+        }
+        //esto es para que no tengan limite de clonacion de cubos.
+        // clon = Instantiate(brickPrefab);
+        //sin el += se clonaria en (-3,0,0) con el +=
+       // clon.transform.position += new Vector3(Xoffset, 0, 0);
+       // Xoffset += 1.1f;
+       // counter++;
+     
+    }
+}
